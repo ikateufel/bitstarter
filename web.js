8c541be12ea947340e2b0fefcbaefde164238586
Juan Carlos <teufelss@gmail.com>
2013-07-10 19:11:04 +0000

    Fifth

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var f_buffer = String(fs.readSync("index.html"));
+	var f_buffer = String(fs.readFileSync("index.html"));
 	response.send(f_buffer);
 });
 
