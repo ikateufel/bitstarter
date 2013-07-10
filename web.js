bd9bfde2c4edec1d580259d9dd127a3abe1710ac
Juan Carlos <teufelss@gmail.com>
2013-07-10 19:03:53 +0000

    Second attempt

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var f_buffer = fs.readSync('./index.html')
+	response.send(f_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
