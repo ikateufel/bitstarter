645fca7fd7426890b31dfe67ebff7fac9e745beb
Juan Carlos <teufelss@gmail.com>
2013-07-10 19:06:54 +0000

    Third attempt

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var f_buffer = fs.readSync('./index.html')
+	var f_buffer = fs.readSync("index.html");
 	response.send(f_buffer.toString());
 });
 
